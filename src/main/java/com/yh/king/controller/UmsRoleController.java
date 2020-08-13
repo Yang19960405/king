9a5bfaf24ca3fcaf8ead378659b02a2f97e5a4bd
yh <495241237@qq.com>
2020-08-13 10:48:51 +0800

    整合swagger接口文档

--- src/main/java/com/yh/king/controller/UmsRoleController.java
+++ src/main/java/com/yh/king/controller/UmsRoleController.java
@@ -3,6 +3,10 @@ package com.yh.king.controller;
 
 import com.yh.king.model.auto.UmsRole;
 import com.yh.king.service.IUmsRoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +25,16 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/umsrole")
+@Api(value = "用户角色类")
 public class UmsRoleController {
 
     @Autowired
     private IUmsRoleService umsRoleService;
 
     @GetMapping("/getUmsPole")
+    @ApiOperation(value = "获取所有用户角色信息",notes = "返回角色List")
+//    @ApiImplicitParam(paramType = "query",name="getUmsRole",value = "角色",required = true)
+//    @ApiResponse(code = 400,message = "请求失败",response = String.class)
     public List<UmsRole> getUmsRole(){
         return umsRoleService.list();
     }
--- src/main/java/com/yh/king/controller/UserAdminController.java
+++ src/main/java/com/yh/king/controller/UserAdminController.java
@@ -2,6 +2,10 @@ package com.yh.king.controller;
 
 import com.yh.king.model.UserAdmin;
 import com.yh.king.service.IUserAdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +15,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/UserAdmin")
+@Api(value = "管理员用户类")
 public class UserAdminController {
     @Autowired
     private IUserAdminService userAdminService;
 
     @GetMapping("/getAll")
+    @ApiOperation(value = "获取所有管理员信息",notes = "返回管理员List")
+//    @ApiImplicitParam(paramType = "query",name="getAllUserAdmin",value = "管理员",required = true)
+//    @ApiResponse(code = 400,message = "请求失败",response = String.class)
     public List<UserAdmin> getAllUserAdmin(){
         return userAdminService.list();
     }
